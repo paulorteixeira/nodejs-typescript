de4162f4c122650e3ea387ce7cd3609c308ae86f
Paulo Ricardo <1981928paulo@gmail.com>
2022-10-21 08:54:07 -0300

    final

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Application} from "express";
+import express, { Application } from 'express';
 import mongoose from 'mongoose';
 import compression from 'compression';
 import cors from 'cors';
@@ -7,11 +7,11 @@ import Controller from '@/utils/interfaces/controller.interface';
 import ErrorMiddleware from '@/middleware/erro.middleware';
 import helmet from 'helmet';
 
-class App{
+class App {
     public express: Application;
     public port: number;
 
-    constructor(controllers:Controller[],port:number){
+    constructor(controllers: Controller[], port: number) {
         this.express = express();
         this.port = port;
 
@@ -21,33 +21,33 @@ class App{
         this.initialiseErrorHandling();
     }
 
-    private initialiseMiddleware(): void{
+    private initialiseMiddleware(): void {
         this.express.use(helmet());
         this.express.use(cors());
         this.express.use(morgan('dev'));
         this.express.use(express.json());
-        this.express.use(express.urlencoded({extended:false}));
+        this.express.use(express.urlencoded({ extended: false }));
         this.express.use(compression());
     }
-    private initialiseControllers(controller: Controller[]): void{
-        controller.forEach((controller:Controller)=>{
+    private initialiseControllers(controller: Controller[]): void {
+        controller.forEach((controller: Controller) => {
             this.express.use('/api', controller.router);
         });
     }
 
-    private initialiseErrorHandling(): void{
+    private initialiseErrorHandling(): void {
         this.express.use(ErrorMiddleware);
     }
 
-    private initialiseDatabaseConnection():void{
-        const {MONGO_USER, MONGO_PASSWORD, MONGO_PATH} = process.env;
+    private initialiseDatabaseConnection(): void {
+        const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
         mongoose.connect(
             `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`
         );
     }
 
-    public listen():void{
-        this.express.listen(this.port,()=>{
+    public listen(): void {
+        this.express.listen(this.port, () => {
             console.log('Listening on port ${this.port}');
         });
     }
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import 'dotenv/config';
 import 'module-alias/register';
 import validadeEnv from '@/utils/interfaces/validadeEnv';
 import App from './app';
+import PostController from '@/resources/post/post.controller';
 
 validadeEnv();
 
-const app = new App([],Number(process.env.PORT));
+const app = new App([
+    new PostController()
+], Number(process.env.PORT));
 
